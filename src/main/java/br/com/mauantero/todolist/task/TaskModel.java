d8acec4f2f386059f27f3071ebe4619509a1972f
Mauricio Antero <mjunior447@gmail.com>
2025-01-24 23:53:01 -0300

    feat: create tasks controller

--- src/main/java/br/com/mauantero/todolist/task/TaskModel.java
+++ src/main/java/br/com/mauantero/todolist/task/TaskModel.java
@@ -1,4 +1,4 @@
-package br.com.mauantero.todolist.user;
+package br.com.mauantero.todolist.task;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -11,16 +11,20 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
-@Entity(name = "tb_users")
-public class UserModel {
+@Entity(name = "tb_tasks")
+public class TaskModel {
     @Id
     @GeneratedValue(generator = "UUID")
     private UUID id;
+    private String description;
 
-    @Column(unique = true)
-    private String username;
-    private String name;
-    private String password;
+    @Column(length = 50)
+    private String title;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private String priority;
+
+    private UUID idUser;
 
     @CreationTimestamp
     private LocalDateTime createdAt;
