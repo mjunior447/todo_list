7214853ae2164dec5ba28b805f2dbf00ed396898
Mauricio Antero <mjunior447@gmail.com>
2025-01-25 22:02:04 -0300

    feat: task owner validation; create error handler for task title

--- src/main/java/br/com/mauantero/todolist/task/TaskModel.java
+++ src/main/java/br/com/mauantero/todolist/task/TaskModel.java
@@ -28,4 +28,11 @@ public class TaskModel {
 
     @CreationTimestamp
     private LocalDateTime createdAt;
+
+    public void setTitle(String title) throws Exception {
+        if (title.length() > 50) {
+            throw new Exception("o campo title deve conter no maximo 50 caracteres");
+        }
+        this.title = title;
+    }
 }
